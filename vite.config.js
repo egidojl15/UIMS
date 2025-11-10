e01074c01a135ce2636f5dc7d9aa6ebf8cb8106a
egidojl15 <160094265+egidojl15@users.noreply.github.com>
2025-11-10 23:03:03 +0800

    Fix

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: process.env.VITE_BASE_PATH || "/UIMS",
+  preview: {
+    host: "0.0.0.0",
+    port: process.env.PORT || 5173,
+    strictPort: false,
+  },
+  build: {
+    outDir: "dist",
+    sourcemap: false,
+  },
+});
