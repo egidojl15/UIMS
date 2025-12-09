1c0f988ed730a8855a24ae7bb256d267ccad6d4d
egidojl15 <160094265+egidojl15@users.noreply.github.com>
2025-12-09 19:21:12 +0800

    uncommented the religion section

--- src/pages/ManageResidentPage.jsx
+++ src/pages/ManageResidentPage.jsx
@@ -3378,7 +3378,7 @@ const ManageResidentsPage = () => {
                 <th className="py-3 px-2 text-left font-semibold border-r border-white/20 sticky top-0 z-10 bg-[#0F4C81] whitespace-nowrap text-xs sm:text-sm min-w-[100px]">
                   Civil Status
                 </th>
-                {/* <th className="py-4 px-2 text-left font-semibold border-r border-white/20 sticky top-0 z-10 bg-[#0F4C81]">
+                <th className="py-4 px-2 text-left font-semibold border-r border-white/20 sticky top-0 z-10 bg-[#0F4C81]">
                   Religion
                 </th>
                 <th className="py-4 px-2 text-left font-semibold border-r border-white/20 sticky top-0 z-10 bg-[#0F4C81]">
@@ -3386,7 +3386,7 @@ const ManageResidentsPage = () => {
                 </th>
                 <th className="py-4 px-2 text-left font-semibold border-r border-white/20 sticky top-0 z-10 bg-[#0F4C81]">
                   Education
-                </th> */}
+                </th>
                 <th className="py-3 px-2 text-center font-semibold border-r border-white/20 sticky top-0 z-10 bg-[#0F4C81] whitespace-nowrap text-xs sm:text-sm min-w-[60px]">
                   PWD
                 </th>
@@ -3447,7 +3447,7 @@ const ManageResidentsPage = () => {
                     <td className="py-3 px-2 border-r border-gray-200 text-xs sm:text-sm whitespace-nowrap">
                       {r.civil_status}
                     </td>
-                    {/* <td className="py-4 px-2 border-r border-gray-200">
+                    <td className="py-4 px-2 border-r border-gray-200">
                       {r.religion || "-"}
                     </td>
                     <td className="py-4 px-2 border-r border-gray-200">
@@ -3455,7 +3455,7 @@ const ManageResidentsPage = () => {
                     </td>
                     <td className="py-4 px-2 border-r border-gray-200">
                       {r.educational_attainment || "-"}
-                    </td> */}
+                    </td>
                     <td className="py-3 px-2 border-r border-gray-200 text-center">
                       <span
                         className={`inline-flex items-center justify-center w-6 h-6 rounded-full text-white text-xs font-bold ${
